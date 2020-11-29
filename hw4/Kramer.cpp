665f932a31299c4e74a08fd0c99ec2504018401e
Maxim <maxgromash19@gmail.com>
2020-11-30 01:47:45 +0500

    Add hw4

---
+++ hw4/Kramer.cpp
@@ -0,0 +1,134 @@
+﻿#include <iostream>
+#include <fstream>
+#include <thread>
+#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <omp.h>
+
+//Метод вывода СЛАУ на экран
+void printSLAU(int arc[4][5]) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (j == 4)
+				std::cout << "= " << arc[i][j] << std::endl;
+			else
+				std::cout << arc[i][j] << "*x" << j + 1 << "\t";
+		}
+	}
+}
+
+//Вычисление определителя 3 на 3
+long long calcDetThreeXThree(int arc[3][3]) {
+	long long det = arc[0][0] * arc[1][1] * arc[2][2] + arc[0][1] * arc[1][2] * arc[2][0] + arc[1][0] * arc[2][1] * arc[0][2];
+	det -= (arc[0][2] * arc[1][1] * arc[2][0] + arc[1][0] * arc[0][1] * arc[2][2] + arc[0][0] * arc[2][1] * arc[1][2]);
+	return det;
+}
+
+//Вычисление определителя 4 на 4
+long long calcDetFourXFour(int arc[4][5]) {
+	long long det = 0;
+	int minor[3][3];
+
+	//Разложение матрицы по первой строке
+	for (int i = 0; i < 4; i++) {
+		for (int j = 1; j < 4; j++)
+		{
+			int ind = 0;
+			//Составление минора
+			for (int k = 0; k < 4; k++)
+			{
+				if (k == i)
+					continue;
+
+				minor[j - 1][ind] = arc[j][k];
+				ind++;
+			}
+		}
+		//Вычисление определителя минора
+		long long minorDet = calcDetThreeXThree(minor);
+		if (i % 2 == 1)
+			minorDet *= -1;
+		det += (long long)arc[0][i] * minorDet;
+	}
+	return det;
+}
+
+int main()
+{
+	int arc[4][5];
+
+	//Считывание входных данных из консоли
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			while (true)
+			{
+				try
+				{
+					std::string s;
+					if (j != 4)
+						std::cout << "a" << i + 1 << j + 1 << " = ";
+					else
+						std::cout << "b" << i + 1 << " = ";
+
+					std::cin >> s;
+					arc[i][j] = std::stoi(s);
+					//Обрабаботка некорректных входных данных
+					if (arc[i][j] >= 1000 || arc[i][j] <= -1000)
+						continue;
+					break;
+				}
+				catch (std::invalid_argument) {
+					continue;
+				}
+			}
+		}
+		std::cout << std::endl;
+	}
+
+
+	//Вывод СЛАУ на экран
+	printSLAU(arc);
+
+	//Высиление детерминанта коэфициентов
+	long long mainDet = calcDetFourXFour(arc);
+
+	//Если детерминант 0 - Крамера использовать нельзя
+	if (mainDet == 0)
+	{
+		std::cout << std::endl << "Main determinant is 0. Can't use Kramer method";
+		return 0;
+	}
+
+	std::cout << std::endl << "SLAU roots: ";
+
+	omp_set_num_threads(4);
+
+	#pragma omp parallel shared(arc, mainDet) 
+	{
+		//Замена стобца коэфициентов свободными членами
+		int arrc[4][5];
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++) {
+				if (j == omp_get_thread_num())
+					arrc[i][j] = arc[i][4];
+				else
+					arrc[i][j] = arc[i][j];
+			}
+		}
+
+		//Вычисление определителя
+		long long det = calcDetFourXFour(arrc);
+
+		//Вывод и вычисление корня, тут образуется очередь на вход
+		#pragma omp critical 
+		{
+			std::cout << (double)det / (double)mainDet << " ";
+		}
+	}
+}
